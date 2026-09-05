13e1fac971fda2107c327faf0602d86b1a80d8a1
NewbieXvwu <87637612+NewbieXvwu@users.noreply.github.com>
2026-09-05 09:33:04 +0800

    feat: add rc1 subagent controller

--- glass/Sources/Core/Controllers/HarnessControllers.swift
+++ glass/Sources/Core/Controllers/HarnessControllers.swift
@@ -4,10 +4,12 @@ struct HarnessControllers: Sendable {
     let sessions: SessionController
     let workspaces: WorkspaceController
     let goals: GoalController
+    let subagents: SubagentController
 
     init(remote: RemoteConnection) {
         sessions = SessionController(remote: remote)
         workspaces = WorkspaceController(remote: remote)
         goals = GoalController(remote: remote)
+        subagents = SubagentController(remote: remote)
     }
 }
